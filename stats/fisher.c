c6c0ae613ec5ab3f5dfbfe7dac77d89770988d10
imedina <imedina@cipf.es>
2012-10-04 13:24:55 +0200

    investigating behaviour of Fisher test

--- stats/fisher.c
+++ stats/fisher.c
@@ -333,7 +333,11 @@ double *fisher_test_omp(int* a, int* b, int* c, int* d, int len, enum Fisher_mod
             }
         }
     }
-    printf("mode: %i, result: %f", mode, result);
+    
+    for (int i = 0; i < len; i++) {
+        printf("result[i]: %f", result[i]);
+    }
+    
     return result;
 }
 
